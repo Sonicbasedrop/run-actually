411eae5dfc8b6df05379d51d9a74525c3f7cc27d
Sonicbasedrop <ivan.ulysses@hotmail.com>
2021-12-06 17:28:54 +0000

    Bind data from db to accordion

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def logout():
 @app.route("/")
 @app.route("/get_events")
 def get_events():
-    events = mongo.db.events.find()
+    events = list(mongo.db.events.find())
     return render_template("events.html", events=events)    
 
 
