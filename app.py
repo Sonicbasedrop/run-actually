72bed8c74eb728993f188dd449e02d1e44d51b65
Sonicbasedrop <ivan.ulysses@hotmail.com>
2021-12-08 12:59:09 +0000

    Wire up event edit button

--- app.py
+++ app.py
@@ -130,6 +130,13 @@ def create_event():
     return render_template("create_event.html", categories=categories)
 
 
+@app.route("/edit_event/<event_id>", methods=["GET", "POST"])
+def edit_event(event_id):
+    event = mongo.db.events.find_one({"_id": ObjectId(event_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_event.html", event=event, categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
