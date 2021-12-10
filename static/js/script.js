55ce31a5c40ae433187c8a6ab92decc937902232
Sonicbasedrop <ivan.ulysses@hotmail.com>
2021-12-10 20:42:53 +0000

    Add comments to code

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,13 @@
-/*
-    jQuery for MaterializeCSS initialization
-*/
-
+ // jQuery for MaterializeCSS initialization
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    // collapsible events
     $('.collapsible').collapsible();
+     // tooltip 
     $(".tooltipped").tooltip();
+    // select options
     $('select').formSelect();
+     // date picker 
     $(".datepicker").datepicker({
         format: "dd mmmm, yyyy",
         yearRange: 3,
@@ -15,16 +16,19 @@ $(document).ready(function(){
             done: "Select"
         }
     });
-    
+    // function to validate materialize form select elements and add different CSS depending on valid or invalid entry
     validateMaterializeSelect();
     function validateMaterializeSelect() {
+        // variable containing CSS for valid option
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        // variable containing CSS for invalid option
         let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
         if ($("select.validate").prop("required")) {
             $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
         }
         $(".select-wrapper input.select-dropdown").on("focusin", function () {
             $(this).parent(".select-wrapper").on("change", function () {
+                // if list item is select that is not disabled ("Choose Option" Default value is) then add isValid CSS 
                 if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
                     $(this).children("input").css(classValid);
                 }
@@ -33,6 +37,7 @@ $(document).ready(function(){
             if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
                 $(this).parent(".select-wrapper").children("input").css(classValid);
             } else {
+                  // else add invalid CSS if above conditions not satisfied as valid entry
                 $(".select-wrapper input.select-dropdown").on("focusout", function () {
                     if ($(this).parent(".select-wrapper").children("select").prop("required")) {
                         if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
@@ -44,3 +49,4 @@ $(document).ready(function(){
         });
     }
 });
+    
