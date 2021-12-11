a8f509e251e2e2a0d48ac34a83f1a8d3ee8c5d21
Sonicbasedrop <ivan.ulysses@hotmail.com>
2021-12-11 23:01:15 +0000

    Fixed issue that prevented contact form to send user info to site owner

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,20 +1,21 @@
 /* Function for contact form to send email with 
     contact form contents to site owner */
-    function sendMail(contactForm) {
-        emailjs.send("service_mygcksp","contact_form", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.emailaddress.value,
-            "comments": contactForm.comments.value
-        })
-        .then(
-            function() {
-                $('.emailaddress-response').html("Thank you for your email, someone will be in touch shortly.");
-            },
-            function() {
-                $('.emailaddress-response').html("There was an error with our email service. Please try again in a few minutes.");
-            }
-        );
-        // Reset Form data
-        document.getElementById("contact-form").reset();
-        return false;
-    }
+function sendMail(contactForm) {
+    emailjs.send("gmail","contact_form", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "comments": contactForm.comments.value
+
+    })
+    .then(
+        function() {
+            $('.response').html("Thank you for your message, have great day!");
+        },
+        function() {
+            $('.response').html("There was an error with our email service. Please try again in a few minutes.");
+        }
+    );
+    // Reset Form data
+    document.getElementById("contact_form").reset();
+    return false;
+}
